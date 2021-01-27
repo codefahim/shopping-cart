811d0d644b8d8026e0e1402451198ec1cdfc36ca
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-01-27 21:50:45 +0600

    Project Completed

---
+++ js/script_v2.js
@@ -0,0 +1,103 @@
+const minus = document.getElementById('minus');
+const plus = document.getElementById('plus');
+const iphone = document.getElementById('iphone');
+
+
+const minusOne = document.getElementById('minusOne');
+const plusOne = document.getElementById('plusOne');
+const iphoneOne = document.getElementById('iphoneOne');
+
+
+minus.addEventListener('click', function() {
+    let inputNumber = getInputNumber('text');
+    if (inputNumber >= 1) {
+        inputNumber--;
+        document.getElementById('text').value = inputNumber;
+
+
+        let phonePrice = getInputText('iphone');
+        phonePrice = inputNumber * 1219;
+        document.getElementById('iphone').innerText = phonePrice;
+    }
+    total();
+})
+
+plus.addEventListener('click', function() {
+    let inputNumber = getInputNumber('text');
+    inputNumber++;
+    document.getElementById('text').value = inputNumber;
+
+
+    let phonePrice = getInputText('iphone');
+    phonePrice = inputNumber * 1219;
+    document.getElementById('iphone').innerText = phonePrice;
+    total();
+})
+
+
+minusOne.addEventListener('click', function() {
+    let inputNumber = getInputNumber('textOne');
+    if (inputNumber >= 1) {
+        inputNumber--;
+        document.getElementById('textOne').value = inputNumber;
+
+
+        let casePrice = getInputText('iphoneOne');
+        casePrice = inputNumber * 59;
+        document.getElementById('iphoneOne').innerText = casePrice;
+    }
+    total();
+})
+
+plusOne.addEventListener('click', function() {
+    let inputNumber = getInputNumber('textOne');
+    inputNumber++;
+    document.getElementById('textOne').value = inputNumber;
+
+
+    let casePrice = getInputText('iphoneOne');
+    casePrice = inputNumber * 59;
+    document.getElementById('iphoneOne').innerText = casePrice;
+    total();
+})
+
+function total() {
+    let phonePrice = getInputText('iphone');
+    document.getElementById('iphone').innerText = phonePrice;
+
+    let casePrice = getInputText('iphoneOne');
+    document.getElementById('iphoneOne').innerText = casePrice;
+
+    let subTotal = getInputText('subTotal');
+    subTotal = phonePrice + casePrice;
+    document.getElementById('subTotal').innerText = subTotal;
+
+
+    let texInput = taxCalculation(subTotal);
+    document.getElementById('tax').innerText = texInput;
+    let totalCost = texInput + subTotal;
+    document.getElementById('total').innerText = totalCost;
+    console.log(texInput);
+
+}
+
+
+function getInputNumber(id) {
+    const textInput = document.getElementById(id).value;
+    let inputNumber = parseFloat(textInput);
+    return inputNumber;
+}
+
+function getInputText(id) {
+    const textInput = document.getElementById(id).innerText;
+    let inputNumber = parseFloat(textInput);
+    return inputNumber;
+}
+
+function taxCalculation(price) {
+    let taxAmount = 0;
+    if (price > 1500) {
+        taxAmount = Math.ceil(price * 0.05);
+    }
+    return taxAmount;
+}
